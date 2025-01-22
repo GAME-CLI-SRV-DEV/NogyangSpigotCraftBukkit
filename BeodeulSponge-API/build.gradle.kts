b3b0a7771b930a689304caffa98ed0d20ed68ab9
Owner Of Approximaster Studios 2004 <sunnybunnies09@naver.com>
2025-01-23 01:56:44 +0900

    Update build.gradle.kts

--- BeodeulSponge-API/build.gradle.kts
+++ BeodeulSponge-API/build.gradle.kts
@@ -13,7 +13,7 @@ repositories {
 }
 
 dependencies {
-    implementation("org.spongepowered:spongeapi:14.0.0-SNAPSHOT") // 14.0.0
-implementation("org.spongepowered:spongeapi:1.21.4-14.0.0-SNAPSHOT") // 14.0.0
+    implementation("org.spongepowered:spongeapi:12.0.0-SNAPSHOT") // 12.0.0
+implementation("org.spongepowered:spongeapi:1.21.1-12.0.0-SNAPSHOT") // 12.0.0
     implementation("net.glowstone:glowstone:2022.6.1-SNAPSHOT") // 2022.6.1
 }
