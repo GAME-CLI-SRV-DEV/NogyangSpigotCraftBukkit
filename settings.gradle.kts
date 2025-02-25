1e6c29c36bbf6948f6bb7d00f229c0aaf25af6a9
Owner Of Approximaster Studios 2004 <sunnybunnies09@naver.com>
2025-02-25 16:50:41 +0900

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,6 +9,6 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.7.0"
 }
 
-rootProject.name = "OneKoreaSpigotCraftBukkit"
+rootProject.name = "NogyangSpigotCraftBukkit"
 
-include("OneKorea-API", "OneKorea-Server")
+include("NogyangSpigot-API", "NogyangSpigotCraftBukkit-Server")
